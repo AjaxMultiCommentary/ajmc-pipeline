c7603ede130797ec49aa3e0e70388e51f7469c8d
sven-nm <49749166+sven-nm@users.noreply.github.com>
2024-07-17 16:11:41 +0200

    Amendments to corpora, notably adding missing objects and cleaning scripts

--- ajmc/corpora/corpora_classes.py
+++ ajmc/corpora/corpora_classes.py
@@ -1,13 +1,18 @@
 import json
 import re
+import shutil
 import typing
+import zipfile
 from abc import ABC, abstractmethod
 from pathlib import Path
 
 import beta_code
 from bs4 import BeautifulSoup
+from pdfminer.high_level import extract_pages
+from pdfminer.layout import LTTextContainer
+from tqdm import tqdm
 
-from ajmc.commons.unicode_utils import is_charset_string
+from ajmc.commons.unicode_utils import is_charset_string, get_char_charset
 from ajmc.corpora import variables as vs
 from ajmc.corpora.cleaning_utils import harmonise_linebreaks
 
@@ -55,7 +60,7 @@ class Corpus(ABC):
 
 
     def get_chunks(self, chunk_size: int, unit: str = 'word') -> typing.List[str]:
-        """Chunks the corpus into string ``chunk-size`` units.
+        """Chunks the corpus into strings of ``chunk-size`` units.
 
         Args:
             chunk_size: The size of the chunks
@@ -70,12 +75,38 @@ class Corpus(ABC):
         else:
             raise NotImplementedError(f'Unit {unit} not implemented ; should be either "word" or "character"')
 
+
+    # Todo
+    def chunk_to_sentences(self, max_length: int = 2048):
+        raise NotImplementedError
+
     def write_plain_text(self, path: typing.Optional[typing.Union[str, Path]] = None):
         if path is None:
             (self.data_dir / 'corpus.txt').write_text(self.get_plain_text(), encoding='utf-8')
         else:
             Path(path).write_text(self.get_plain_text(), encoding='utf-8')
 
+
+    @staticmethod
+    def is_valid_greek(text: str, stopwords_threshold: int = 2, greek_chars_threshold: float = 0.001) -> bool:
+        total_greek_chars = len([c for c in text if get_char_charset(c) == 'greek'])
+        latinised_greek_stopwords = ['kai', 'kal', 'tovto', 'tov', 'yap']
+
+        # if total_greek_chars/len(text) < 0.01:
+        stopwords_count = sum([1 for word in text.split() if word.lower() in latinised_greek_stopwords])
+        greek_chars_ratio = total_greek_chars / len(text)
+
+        # Case 1: Low Greek chars, low stopwords
+        if greek_chars_ratio < greek_chars_threshold and stopwords_count < stopwords_threshold:
+            return True
+        # Case 2: Low Greek chars, high stopwords
+        elif greek_chars_ratio < greek_chars_threshold and stopwords_count >= stopwords_threshold:
+            return False
+        # Case 3: High Greek chars, low stopwords
+        elif greek_chars_ratio >= greek_chars_threshold and stopwords_count < stopwords_threshold + 10:
+            return True
+        else:
+            return False
 
 class PlainTextCorpus(Corpus):
 
@@ -92,6 +123,94 @@ class PlainTextCorpus(Corpus):
 
     def get_plain_text(self) -> str:
         return self.files[0].read_text(encoding='utf-8')
+
+
+class PdfCorpus(PlainTextCorpus):
+    def __init__(self, corpus_id: str):
+        super().__init__(corpus_id)
+        self.raw_texts_dir = self.root_dir / 'raw_texts'
+        self.data_dir.mkdir(exist_ok=True)
+        self.raw_texts_dir.mkdir(exist_ok=True)
+        self.pdf_dir = self.root_dir / 'pdfs'
+
+    @staticmethod
+    def parse_pdf(pdf_path: Path):
+        """Parses a pdf file and returns the text, using pdfminer.six.
+
+        Warning:
+            This function does not handle the extraction of greek characters properly all the time.
+
+        Args:
+            pdf_path: The path to the pdf file to parse
+        """
+        text = ''
+        for page_layout in extract_pages(pdf_path):
+            for element in page_layout:
+                if isinstance(element, LTTextContainer):
+                    text += element.get_text() + '\n'
+        return text
+
+    def extract_texts(self, **kwargs):
+        """Extracts the text from the pdfs and writes it to files in the raw_texts_dir"""
+        for pdf_path in tqdm(sorted(self.pdf_dir.rglob('*.pdf')), desc='Extracting texts', total=len(list(self.pdf_dir.rglob('*.pdf')))):
+            if (self.raw_texts_dir / (pdf_path.stem + '.txt')).exists():
+                continue
+            try:
+                text = self.parse_pdf(pdf_path)
+                (self.raw_texts_dir / (pdf_path.stem + '.txt')).write_text(text, encoding='utf-8')
+            except Exception as e:
+                print(f'Error while parsing {pdf_path}: {e}')
+                continue
+
+
+    def clean_texts(self, stopwords_threshold: int = 2, greek_chars_threshold: float = 0.001):
+
+        total_text = ''
+        deleted = 0
+        for txt in tqdm(self.raw_texts_dir.glob('*.txt')):
+            text = txt.read_text(encoding='utf-8')
+            if not self.is_valid_greek(text, stopwords_threshold, greek_chars_threshold):
+                deleted += 1
+            else:
+                total_text += text + '\n\n\n'
+
+        print(f'{deleted} documents deleted')
+        (self.data_dir / 'corpus_cleaned.txt').write_text(total_text, encoding='utf-8')
+
+
+class BrillCorpus(PdfCorpus):
+
+    def __init__(self, corpus_id: str):
+        super().__init__(corpus_id)
+
+    def extract_texts(self, source_dir: Path, tmp_dir: Path):
+        shutil.rmtree(tmp_dir, ignore_errors=True)
+        tmp_dir.mkdir(exist_ok=True)
+        self.raw_texts_dir.mkdir(exist_ok=True)
+
+        # Read a zip file
+        for zip_path in tqdm(list(source_dir.glob('*.zip')), desc='Extracting texts'):
+            with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+                zip_ref.extractall(tmp_dir)
+
+            for sub_zip_path in tmp_dir.rglob('*.zip'):
+                print('Subzips found:', sub_zip_path)
+                sub_zip_dir = tmp_dir / sub_zip_path.stem
+                sub_zip_dir.mkdir(exist_ok=True)
+                with zipfile.ZipFile(sub_zip_path, 'r') as zip_ref:
+                    zip_ref.extractall(sub_zip_dir)
+
+            text = ''
+            for pdf_path in sorted(tmp_dir.rglob('*.pdf')):
+                for page_layout in extract_pages(pdf_path):
+                    for element in page_layout:
+                        if isinstance(element, LTTextContainer):
+                            text += element.get_text() + '\n'
+
+            (self.raw_texts_dir / (zip_path.stem + '.txt')).write_text(text, encoding='utf-8')
+
+            shutil.rmtree(tmp_dir, ignore_errors=True)
+            tmp_dir.mkdir(exist_ok=True)
 
 
 class TeiCorpus(Corpus):
